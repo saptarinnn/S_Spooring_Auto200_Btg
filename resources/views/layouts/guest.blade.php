146d8684cf2dc5f2d56c73194c2778ed3259a065
saptanaaa <saptarino708@gmail.com>
2024-02-03 05:37:22 +0800

    comp[script]: add script component

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,8 +12,7 @@
     {{ $slot }}
 
     {{-- Script --}}
-    <script src="{{ asset('js/preline.js') }}"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <x-script />
 </body>
 
 </html>
