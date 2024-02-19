9fc603aa4a3a3bfbcb2befaeac7faf7a5df3828f
saptanaaa <saptarino708@gmail.com>
2024-02-19 14:01:30 +0800

    refactor[routes]: grouping routes with roles

--- routes/web.php
+++ routes/web.php
@@ -34,33 +34,70 @@ Route::post('detail-spooring-confirm/{id}', [HomeController::class, 'confirm'])-
 Route::middleware('auth')->group(function () {
     # Dashboard
     Route::get('dashboard', fn () => view('master.dashboard'))->name('dashboard');
-
     # Booking
     Route::resource('booking', BookingController::class)->except('edit', 'create', 'store');
-
     # Konfirasi Spooring
     Route::post('spooring-confirm/{id}', [SpooringController::class, 'confirm'])->name('spooring.confirm');
-    Route::resource('spooring', SpooringController::class)->only('index', 'show', 'edit', 'udpate');
-
+    Route::resource('spooring', SpooringController::class)->only('index', 'show', 'edit', 'update');
     # Homepages
     Route::resource('homepages', HomepageController::class)->only('index', 'store');
-
     # Pegguna
     Route::resource('users', UserController::class)->except('show');
-
     # Barang
     Route::resource('barang', BarangController::class)->except('show');
-
     # Barang Masuk
     Route::resource('brg-masuk', BarangMasukController::class)->except('show', 'edit', 'update');
-
     # Barang Keluar
     Route::resource('brg-keluar', BarangKeluarController::class)->only('index', 'create', 'store', 'destroy');
 });
 
 # Admin
 Route::middleware(['auth', 'role:admin'])->group(function () {
+    # Homepages
+    Route::resource('homepages', HomepageController::class)->only('index', 'store');
+    # Pengguna
+    Route::resource('users', UserController::class)->only('index', 'create', 'store', 'edit', 'update', 'destroy');
+    # Booking
+    Route::resource('booking', BookingController::class)->only('index');
+    # Konfirmasi Spooring
+    Route::resource('spooring', SpooringController::class)->only('index');
+    # Barang
+    Route::resource('barang', BarangController::class)->only('index', 'create', 'store', 'edit', 'update', 'destroy');
+    # Barang Masuk
+    Route::resource('brg-masuk', BarangMasukController::class)->only('index', 'create', 'store', 'destroy');
+    # Barang Keluar
+    Route::resource('brg-keluar', BarangKeluarController::class)->only('index', 'create', 'store', 'destroy');
+});
 
+# Kepala Bengkel
+Route::middleware(['auth', 'role:kbengkel'])->group(function () {
+    # Barang
+    Route::resource('barang', BarangController::class)->only('index');
+    # Barang Masuk
+    Route::resource('brg-masuk', BarangMasukController::class)->only('index');
+    # Barang Keluar
+    Route::resource('brg-keluar', BarangKeluarController::class)->only('index');
+});
+
+# Mekanik
+Route::middleware(['auth', 'role:mekanik'])->group(function () {
+    # Booking
+    Route::resource('booking', BookingController::class)->except('edit', 'create', 'store');
+    # Konfirasi Spooring
+    Route::post('spooring-confirm/{id}', [SpooringController::class, 'confirm'])->name('spooring.confirm');
+    Route::resource('spooring', SpooringController::class)->only('index', 'show', 'edit', 'update');
+    # Barang
+    Route::resource('barang', BarangController::class)->only('index');
+});
+
+# Adm. Gudang
+Route::middleware(['auth', 'role:gudang'])->group(function () {
+    # Barang
+    Route::resource('barang', BarangController::class)->except('show');
+    # Barang Masuk
+    Route::resource('brg-masuk', BarangMasukController::class)->except('show', 'edit', 'update');
+    # Barang Keluar
+    Route::resource('brg-keluar', BarangKeluarController::class)->only('index', 'create', 'store', 'destroy');
 });
 
 # Authenticate
