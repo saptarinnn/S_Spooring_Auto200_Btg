45d39c7635068966df61fce51474d89ca588dc5a
saptarinnn <saptarino708@gmail.com>
2024-03-06 17:24:42 +0800

    feat: memperbaharui data pengguna tampilan index

--- resources/views/master/users/index.blade.php
+++ resources/views/master/users/index.blade.php
@@ -16,15 +16,19 @@
 
             <x-master.table-tbody>
                 @foreach ($users as $user)
-                <tr>
-                    <x-master.table-tbody-list name="{{ ucwords($user->username) }}" />
-                    <x-master.table-tbody-list name="{{ ucwords($user->email) }}" />
-                    <x-master.table-tbody-list name="{{ ucwords($user->fullname) }}" />
-                    <x-master.table-tbody-list
-                        name="{{ ($user->lastlogin) ? \Carbon\Carbon::parse($user->lastlogin)->format('d M, Y') : '-' }}" />
-                    <x-master.table-tbody-action edit="{{ route('users.edit', $user->id) }}"
-                        delete="{{ route('users.destroy', $user->id) }}" />
-                </tr>
+                    <tr>
+                        <x-master.table-tbody-list name="{{ ucwords($user->username) }}" />
+                        <x-master.table-tbody-list name="{{ ucwords($user->email) }}" />
+                        <x-master.table-tbody-list name="{{ ucwords($user->fullname) }}" />
+                        <x-master.table-tbody-list
+                            name="{{ $user->lastlogin ? \Carbon\Carbon::parse($user->lastlogin)->format('d M, Y') : '-' }}" />
+                        @if ($user->roles[0]->name != 'admin')
+                            <x-master.table-tbody-action edit="{{ route('users.edit', $user->id) }}"
+                                delete="{{ route('users.destroy', $user->id) }}" />
+                        @else
+                            <td>-</td>
+                        @endif
+                    </tr>
                 @endforeach
             </x-master.table-tbody>
         </x-master.table>
@@ -32,49 +36,49 @@
     </section>
 
     @push('scripts')
-    @if(session()->has('message'))
-    <script>
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-                toast.onmouseenter = Swal.stopTimer;
-                toast.onmouseleave = Swal.resumeTimer;
-            }
-        });
-        Toast.fire({
-            icon: "success",
-            title: `{{ session()->get('message') }}`,
-        });
-    </script>
-    @endif
+        @if (session()->has('message'))
+            <script>
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: `{{ session()->get('message') }}`,
+                });
+            </script>
+        @endif
 
-    <script>
-        /* Delete Button */
-        $('.delete-button').click(function (e) {
-            e.preventDefault();
-            let form =  $(this).closest("form");
-            Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!"
+        <script>
+            /* Delete Button */
+            $('.delete-button').click(function(e) {
+                e.preventDefault();
+                let form = $(this).closest("form");
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
                 }).then((result) => {
-                if (result.isConfirmed) {
-                    form.submit();
-                }
+                    if (result.isConfirmed) {
+                        form.submit();
+                    }
+                });
             });
-        });
 
-        /* DataTables */
-        let table = new DataTable('#data-table');
-    </script>
+            /* DataTables */
+            let table = new DataTable('#data-table');
+        </script>
     @endpush
 
 </x-app-layout>
